dc70d501980104bc030658bcde6096a291d42519
Adrian Melo <amelo2@fvi.edu>
2017-01-19 22:04:50 -0500

    completed quiz

---
+++ filter.js
@@ -0,0 +1,9 @@
+// filter
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+ var result = numbers.filter(function(num) {
+   return(num2===8);
+ });
+ console.log(result);
---
+++ reduce.js
@@ -0,0 +1,9 @@
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var result = numbers.reduce(function(var1, var2) {
+  return var1+var2;
+});
+
+console.log(result);
